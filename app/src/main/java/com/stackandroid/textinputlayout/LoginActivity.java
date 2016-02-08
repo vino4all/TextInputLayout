5c42826ccf6402e2a94682a86709b735e674bc41
Vinothkumar <vino4all@gmail.com>
2016-02-08 22:00:41 +0530

    artifacts added

--- app/src/main/java/com/stackandroid/textinputlayout/LoginActivity.java
+++ app/src/main/java/com/stackandroid/textinputlayout/LoginActivity.java
@@ -22,10 +22,6 @@ import android.widget.TextView;
  */
 public class LoginActivity extends AppCompatActivity {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
      * A dummy authentication store containing known user names and passwords.
